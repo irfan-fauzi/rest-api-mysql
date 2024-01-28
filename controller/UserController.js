7d7573cc3ba217a56daa00dd5605e52dc4729159
irfan-fauzi <irfanwintech@yahoo.com>
2024-01-28 17:27:12 +0700

    semua routes berhasil di test

--- controller/UserController.js
+++ controller/UserController.js
@@ -9,3 +9,51 @@ export const getUsers = async (req, res) => {
     console.log(error.message);
   }
 };
+
+export const getUserById = async (req, res) => {
+  try {
+    const response = await User.findOne({
+      where: {
+        id: req.params.id,
+      },
+    });
+    res.status(200).json(response);
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+export const createUser = async (req, res) => {
+  try {
+    await User.create(req.body)
+    res.status(201).json({msg: "User Created Succefuly"});
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+export const updateUser = async (req, res) => {
+  try {
+    await User.update(req.body, {
+      where: {
+        id: req.params.id
+      }
+    })
+    res.status(200).json({msg: "User Updated Succesfuly"});
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+export const deleteUser = async (req, res) => {
+  try {
+    await User.destroy({
+      where: {
+        id: req.params.id
+      }
+    })
+    res.status(200).json({msg: "User was deleted succesfully"});
+  } catch (error) {
+    console.log(error.message);
+  }
+};
--- routes/UserRoute.js
+++ routes/UserRoute.js
@@ -1,8 +1,12 @@
 import express from "express";
-import { getUsers } from "../controller/UserController.js";
+import { createUser, deleteUser, getUserById, getUsers, updateUser } from "../controller/UserController.js";
 
 const router = express.Router();
 
 router.get("/users", getUsers);
+router.get("/users/:id", getUserById);
+router.post("/users", createUser);
+router.patch("/users/:id", updateUser);
+router.delete("/users/:id", deleteUser);
 
 export default router;
